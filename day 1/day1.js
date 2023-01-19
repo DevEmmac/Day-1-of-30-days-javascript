c114d5834f3aa36f0dc197a73a6c961af9c923de
DevEmman <eolasunkanmi@gmail.com>
2023-01-19 07:29:41 -0500

    JavaScript
    30days of javascript, day1

---
+++ day 1/day1.js
@@ -0,0 +1,119 @@
+/*
+ (Question 1) Write a single line comment which says,
+ comment can make code readable
+ */
+
+  // comments can make code readable
+ 
+  /*
+   (Question 2). Write another single comment whih says, Welcome to 30DaysOfJavaScript
+  */
+ 
+  //this is another single comment whih says, 30DaysOfJavaScript
+
+  /*
+  (Question 3). Write a multiline comment which says, comments can make code readable,
+  easy to reuse and informative.
+  */
+
+  /*
+  this is a multline comment,comments can make code readable
+  reuse and informative.
+   */
+
+  /*
+  (Question 4) Create a variable.js file and declare variables and assign string,
+  boolean,underfined and null data types
+  */
+  
+  // assign a string
+
+  let javaScript = 'string';
+
+  // assign a boolean
+
+  let setBe = true;
+
+  // assign undefined
+
+  let coding;
+
+  // assign null
+
+  let letSee = null;
+
+   /*
+    (Question 5) Create dataypes.js file and use the javaScript typeof operator to check different data
+     types. check the data types of each variable
+   */
+    
+     const checkTHis = 'Mark';  
+     console.log(typeof checkTHis);   // string
+
+     const setbeCheck = true;
+     console.log(typeof setbeCheck);  // boolean
+
+     let codebe;
+     console.log(typeof codebe);     // undefined
+
+     let seeNull = '0';           // null
+ 
+
+
+
+
+  /*
+  (Question 6). Declear four variable without assigning values
+   */
+
+  let myLaptopModel;
+  let myschool;
+  let myState;
+  let mykind;
+
+  /*
+   (Question 7). Declear four variables with
+    assigned values
+  */
+
+    const myName = 'Emmananuel';
+    const myNumber = 25;
+    const javaS = true;
+    const java = false;
+
+    /*
+    (Question 8). Declear variable to store your first name, last name, marital status,country and
+    age in multiple lines
+    */
+      
+    
+    const myFirstName = "Emmanuel";
+    const lastName = 'olasunkanmi';
+    const maritalStatus = 'single';
+    const myCountry = 'Nigeria';
+    const myAges = 25;
+    
+    /*
+    (Question 9). Declear variable to store your first name,last name,marital status
+    country and age im a single line
+    */
+    
+    const FirstName = "Emmanuel";      // my first name
+    const mylastName = 'olasunkanmi'; // my second name
+    const maritalsStatus = 'single'; // my marital status is single
+    const country = 'Nigeria';      // my country is nigeria
+    const age = 25;                // age in years
+
+    /*
+    (Question 10). Declare two variable myAge and yourAge and assign them initial
+    values and log to the browser console.
+     */
+       
+      const myAge = 25;
+
+      console.log(myAge);
+
+      const yourAge = 29;
+      
+      console.log(yourAge);
+
