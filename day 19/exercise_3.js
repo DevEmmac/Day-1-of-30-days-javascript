45c7f98b3a7a8edc55f36124f7aa20e63423ae3f
DevEmman <eolasunkanmi@gmail.com>
2023-04-16 18:11:22 -0400

    30-DAYS-OF-JAVASCRIPT
    readme

--- day 19/exercise_3.js
+++ day 19/exercise_3.js
@@ -4,50 +4,53 @@
 Create a personAccount out function. It has firstname, lastname, 
 incomes, expenses inner variables. It has totalIncome, totalExpense,
  accountInfo,addIncome, addExpense and accountBalance inner 
- functions. Incomes is a set of incomes and its description and
+ functions. Incomes is     }
+    }, 2000);a set of incomes and its description and
   expenses is also a set of expenses and its description.
 */
  
-//  function personAccount () {
-//     const firstName = 'Olayiwola'
-//     const lastName = 'Emmanuel'
-//     let incomes = [100, 300, 550]
-//     let expenses = [400]
-//     let totalIncome = 0
-//     let totalExpense = 0
 
-//     const totalIncom() {
-//       incomes.forEach((income) => {
-//         totalIncome += income
-//         return totalIncome
-//       })
-//     }
+function personAccount () {
+  const firstName = 'Olayiwola'
+  const lastName = 'Emmanuel'
+  let incomes = [100, 300, 550]
+  let expenses = [50]
+  let totalIncome = 0
+  let totalExpense = 0
 
-//     const totalExpens() {
-//         expenses.forEach((expense) => {
-//             totalExpens += expense
-//             return totalExpense
-//         })
-//     }
+  function totalIncom () {
+    incomes.forEach((income) => {
+      totalIncome += income
+      return totalIncome
+    })
+  }
 
-//     const accountInfo () {
-//         const personAcctInfo = `full Name: ${firstName} ${lastName}
-//         Total Income: ${totalIncom}
-//         Total Expenses: ${totalExpens}
-//         Banlance: ${accountBalnce()}`
-//         return personAcctInfo
-//     }
-//     const addIncome (incomes) {
-//         incomes.push(incomes)
-//         return incomes
-//     }
-//     const addExpense(expense) {
-//         expenses.push(expense)
-//         return expenses
-//     }
-//     const accountBalance () {
-//         let balance = `${}`
-//     }
-//     return totalIncome(), totalExpense(), accountBalnce(), accountInfo()
-//  }
-// x
+  function totalExpens() {
+      expenses.forEach((expense) => {
+          totalExpense += expense
+          return totalExpense
+      })
+  }
+
+  function accountInfo () {
+      const personAcctInfo = `full Name: ${firstName} ${lastName}
+      Total Income: ${totalIncome}
+      Total Expenses: ${totalExpense}
+      Balance: ${accountBalance()}`
+      return personAcctInfo
+  }
+  function addIncome (incomes) {
+      incomes.push(incomes)
+      return incomes
+  }
+  function addExpense(expense) {
+      expenses.push(expense)
+      return expenses
+  }
+  function accountBalance () {
+      let myBalance = `${totalIncome - totalExpense}`
+      return myBalance
+  }
+  return totalIncom(), totalExpens(), accountBalance(), accountInfo()
+}
+console.log(personAccount())
