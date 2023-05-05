6d7c8c597ede9d9124c261f864607631f7bd3b33
DevEmman <eolasunkanmi@gmail.com>
2023-05-05 12:57:14 -0400

    30-DAYS-OF-JAVASCRUIPT
    exercise

--- day 22/exercise_1/variable01.js
+++ day 22/exercise_1/variable01.js
@@ -6,34 +6,61 @@ B. Odd numbers background is yellow
 C. Prime numbers background is red
 */
 
-const container = document.querySelector('.container')
+let container = document.querySelector('.container')
 container.style.display = 'flex'
 container.style.flexDirection = 'column'
 container.style.alignItems = 'center'
 
-const h1 = document.querySelector('h1')
+let h1 = document.querySelector('h1')
 h1.style.fontSize = '60px'
 h1.style.fontFamily = 'arial'
 h1.style.fontWeight = 'bold'
 h1.style.marginBottom = '-30px'
 
-const h2 = document.querySelector('h2')
+let h2 = document.querySelector('h2')
 h2.style.fontSize = '45px'
 h2.style.fontFamily = 'outfit'
 h2.style.fontWeight = '300'
 h2.style.textDecoration = 'underline'
 
-const p = document.querySelector('p')
+let p = document.querySelector('p')
 p.style.fontSize = '37px'
 p.style.fontFamily = 'outfit'
 p.style.textDecoration = 'underline'
 p.style.marginTop = '-35px'
 
-const containerGrid = document.querySelector('.gridcontainer')
-containerGrid.style.display = 'inline-grid'
-containerGrid.style.gridTemplateColumns = 'repeat(12, 1fr)'
-containerGrid.style.gridTemplateRows = 'repeat(2, 50px)'
-containerGrid.style.gridGap = '5px'
+let containerGrid = document.querySelector('.gridcontainer')
+containerGrid.style.display = 'grid'
+containerGrid.style.gridTemplateColumns = 'repeat(6, 1fr)'
 containerGrid.style.padding = '5px'
-containerGrid.style.border = '2px solid green'
-containerGrid.style.height = '50px'
+containerGrid.style.gridGap = '5px'
+function isPrime(num) {
+    if(num < 2) return false;
+  
+    for (let k = 2; k < num; k++){
+      if( num % k == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+    for (let i = 0; i <= 101; i++) {
+        let containerChild = document.createElement('div')
+        containerChild.textContent = i
+        if (isPrime(i)){
+                containerChild.style.backgroundColor = 'red'
+            }else if (i%2 ===0){
+                containerChild.style.backgroundColor = 'green'
+            }else {
+                containerChild.style.backgroundColor = 'yellow'
+            }
+        containerChild.style.padding= '10px 20px'
+        containerChild.style.textAlign = 'center'
+        containerChild.style.color = 'white'
+        containerChild.style.fontSize = '30px'
+        containerGrid.appendChild(containerChild)
+    }
+
+
---
+++ day 22/exercise_2/country.js
@@ -0,0 +1,34 @@
+// EXERCISE: LEVEL 22
+
+/* Use the countries array to display all the countries.See the design */
+
+let countriesArray = document.querySelector('.countriesArray')
+countriesArray.style.display = 'flex'
+countriesArray.style.flexDirection = 'column'
+countriesArray.style.alignItems = 'center'
+
+
+let h1 = document.querySelector('h1')
+h1.style.fontSize = '60px'
+h1.style.fontFamily = 'Sans-serif'
+h1.style.fontWeight = 'bold'
+
+
+let h2 = document.querySelector('h2')
+h2.style.fontSize = '40px'
+h2.style.fontFamily = 'Sans-serif'
+h2.style.fontWeight = 'bold'
+h2.style.marginTop = '-30px'
+
+let h3 = document.querySelector('h3')
+h3.style.fontSize = '25px'
+h3.style.fontFamily = 'Sans-serif'
+h3.style.fontWeight = '300'
+h3.style.marginTop = '-33px'
+
+let p = document.querySelector('p')
+p.style.fontSize = '25px'
+p.style.fontFamily = 'Sans-serif'
+p.style.marginTop = '-27px'
+
+
